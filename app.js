060cee3762c6856c589642411b434fa77a498012
tobihaze <neko_3shogi@yahoo.co.jp>
2017-08-24 12:09:06 +0000

    コメントを削除

--- app.js
+++ app.js
@@ -6,15 +6,11 @@ const rl = readline.createInterface({ 'input': rs, 'output': {} });
 const map = new Map(); // key: 都道府県 value: 集計データのオブジェクト
 
 rl.on('line', (lineString) => {
-    ////console.log(lineString);
     const columns = lineString.split(',');
     const year = parseInt(columns[0]);
     const prefecture = columns[2];
     const popu = parseInt(columns[7]);
     if (year === 2010 || year === 2015) {
-        ///console.log(year);
-        ///console.log(prefecture);
-        ///console.log(popu);
         let value = map.get(prefecture);
         if (!value) {
             value = {
@@ -38,7 +34,6 @@ rl.on('close', () => {
         const value = pair [1];
         value.change = value.p15 / value.p10;
     }
-    //console.log(map);
     const rankingArray = Array.from(map).sort((p1,p2) => {
         return p2[1].change - p1[1].change;
     });
